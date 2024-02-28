f25e098498bf27ff6a10512be8f644bb272658ae
landocodes <blagrove87@gmail.com>
2024-02-28 01:35:57 -0500

    update config

--- app/config.py
+++ app/config.py
@@ -7,16 +7,15 @@ load_dotenv()
 class Config(object):
     """Base Config Object"""
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY')
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     # MAIL_PORT = os.environ.get('MAIL_PORT')
     # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     # MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'localhost')
+    MAIL_PORT = os.environ.get('MAIL_PORT', '25')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', '').lower() in ['true', '1']
